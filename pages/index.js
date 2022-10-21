a5313e3cb406f8ce6ce0fb8ee6191ebd2ea716d0
techstack-io <81205746+techstack-io@users.noreply.github.com>
2022-10-20 23:03:47 -0500

    Init commit

---
+++ pages/index.js
@@ -0,0 +1,212 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <header>
+      <Head>
+        <title>Zen Hero</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <section class="pt-24 pb-36 bg-white overflow-hidden">
+        <div class="container px-4 mx-auto">
+          <h2 class="mb-4 text-6xl md:text-7xl text-center font-bold font-heading font-heading tracking-px-n leading-tight">
+            Latest from Zen Hero
+          </h2>
+          <p class="mb-24 font-medium text-gray-600 text-center leading-relaxed md:max-w-lg mx-auto">
+            Lorem ipsum dolor sit amet, to the consectr adipiscing elit.
+            Volutpat tempor to the condimentum vitae vel purus.
+          </p>
+          <div class="lg:mx-60 flex flex-wrap -m-8">
+            <div class="w-full md:w-1/2 p-8">
+              <div class="flex flex-wrap lg:items-center -m-4">
+                <div class="w-auto p-4">
+                  <div class="overflow-hidden rounded-xl">
+                    <img
+                      src="img/mind-consciousness.svg"
+                      alt="image"
+                      loading="lazy"
+                      width="100"
+                      height="100"
+                    />
+                  </div>
+                </div>
+                <div class="flex-1 p-4">
+                  <div class="md:max-w-xs">
+                    <div class="flex flex-col justify-between h-full">
+                      <div class="mb-6">
+                        <p class="mb-1.5 text-sm text-gray-500 font-medium uppercase tracking-px">
+                          Mind Consciousness
+                        </p>
+                        <a
+                          class="inline-block hover:text-gray-800 hover:underline"
+                          href="#"
+                        >
+                          <h3 class="text-xl font-semibold leading-normal">
+                            An IT services company that can grow your business
+                          </h3>
+                        </a>
+                      </div>
+                      <div class="flex flex-wrap items-center -m-1">
+                        <div class="w-auto p-1">
+                          <img
+                            src="flaro-assets/images/blog/blog-avatar.png"
+                            alt=""
+                          />
+                        </div>
+                        <div class="w-auto p-1">
+                          <p class="text-sm font-semibold leading-relaxed">
+                            Wade Warren
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="w-full md:w-1/2 p-8">
+              <div class="flex flex-wrap lg:items-center -m-4">
+                <div class="w-auto p-4">
+                  <div class="overflow-hidden rounded-xl">
+                    <img
+                      src="img/sense-consciousness.svg"
+                      alt="image"
+                      loading="lazy"
+                      width="100"
+                      height="100"
+                    />
+                  </div>
+                </div>
+                <div class="flex-1 p-4">
+                  <div class="md:max-w-xs">
+                    <div class="flex flex-col justify-between h-full">
+                      <div class="mb-6">
+                        <p class="mb-1.5 text-sm text-gray-500 font-medium uppercase tracking-px">
+                          Sense Counsciousness
+                        </p>
+                        <a
+                          class="inline-block hover:text-gray-800 hover:underline"
+                          href="#"
+                        >
+                          <h3 class="text-xl font-semibold leading-normal">
+                            How to handle your good employee for the business
+                          </h3>
+                        </a>
+                      </div>
+                      <div class="flex flex-wrap items-center -m-1">
+                        <div class="w-auto p-1"></div>
+                        <div class="w-auto p-1">
+                          <p class="text-sm font-semibold leading-relaxed">
+                            Bessie Cooper
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="w-full md:w-1/2 p-8">
+              <div class="flex flex-wrap lg:items-center -m-4">
+                <div class="w-auto p-4">
+                  <div class="overflow-hidden rounded-xl">
+                    <img
+                      src="img/store-consciousness.svg"
+                      alt="image"
+                      loading="lazy"
+                      width="100"
+                      height="100"
+                    />
+                  </div>
+                </div>
+                <div class="flex-1 p-4">
+                  <div class="md:max-w-xs">
+                    <div class="flex flex-col justify-between h-full">
+                      <div class="mb-6">
+                        <p class="mb-1.5 text-sm text-gray-500 font-medium uppercase tracking-px">
+                          Store Consciousness
+                        </p>
+                        <a
+                          class="inline-block hover:text-gray-800 hover:underline"
+                          href="#"
+                        >
+                          <h3 class="text-xl font-semibold leading-normal">
+                            New consulting for all kind offer related to IT
+                            services
+                          </h3>
+                        </a>
+                      </div>
+                      <div class="flex flex-wrap items-center -m-1">
+                        <div class="w-auto p-1">
+                          <img
+                            src="flaro-assets/images/blog/blog-avatar3.png"
+                            alt=""
+                          />
+                        </div>
+                        <div class="w-auto p-1">
+                          <p class="text-sm font-semibold leading-relaxed">
+                            Albert Flores
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="w-full md:w-1/2 p-8">
+              <div class="flex flex-wrap lg:items-center -m-4">
+                <div class="w-auto p-4">
+                  <div class="overflow-hidden rounded-xl">
+                    <img
+                      src="img/manas-consciousness.svg"
+                      alt="image"
+                      loading="lazy"
+                      width="100"
+                      height="100"
+                    />
+                  </div>
+                </div>
+                <div class="flex-1 p-4">
+                  <div class="md:max-w-xs">
+                    <div class="flex flex-col justify-between h-full">
+                      <div class="mb-6">
+                        <p class="mb-1.5 text-sm text-gray-500 font-medium uppercase tracking-px">
+                          Manas Consciousness
+                        </p>
+                        <a
+                          class="inline-block hover:text-gray-800 hover:underline"
+                          href="#"
+                        >
+                          <h3 class="text-xl font-semibold leading-normal">
+                            How life insurance helps you during financial
+                            insolvency
+                          </h3>
+                        </a>
+                      </div>
+                      <div class="flex flex-wrap items-center -m-1">
+                        <div class="w-auto p-1">
+                          <img
+                            src="flaro-assets/images/blog/blog-avatar4.png"
+                            alt=""
+                          />
+                        </div>
+                        <div class="w-auto p-1">
+                          <p class="text-sm font-semibold leading-relaxed">
+                            Jerome Bell
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </header>
+  );
+}
