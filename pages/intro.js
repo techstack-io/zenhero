62e41b4adeb172583b769fe6cfce05b1729349af
Dan <support@stackmetric.io>
2022-10-24 14:22:28 -0500

    Added to intro

--- pages/intro.js
+++ pages/intro.js
@@ -10,32 +10,61 @@ export default function Intro() {
           <div class="">
             <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2 gap-4 ">
               <div class="">
-                <h1 className="lg:text-4xl text-left text-2xl font-titan-one text-pure-blue">
-                  Transforming The Mind
+                <h1 className="lg:text-4xl text-left text-2xl text-bold text-pure-blue">
+                Transforming The Mind
                 </h1>
                 <br />
                 <p>
-                  At the core of Buddhism are The Four Noble Truths: suffering
-                  exists, there is a cause of suffering, there is an end to
-                  suffering, and there is a path that leads to the end of
-                  suffering.
-                  <br />
-                  <br />
-                  There are many Buddhist sects that exist, but the one we will
-                  concentrate on is Mahayana Buddhism. Unlike other Buddhists,
-                  Mahayana followers look to liberate themselves from suffering
-                  as well as lead other people toward liberation and
-                  enlightenment. Lojong is a set of techiniques for training the
-                  mind. These techniques are designed to open our hearts and
-                  awaken our minds. There are fifty nine slogans and they offer
-                  us a lot of help in transcending our egotism and putting down
-                  the baggage we are carrying. The fifty nine slogans have been
-                  used by Tibetan Buddhists for centuries in order to help
-                  Buddhist practitioners focus on what’s important in our
-                  efforts to train and tame our minds. Sometimes on the Buddhist
-                  path we can tend to forget why we’re doing this and what’s
-                  important.
+                  At the heart of the Zen Hero philosophy is the practice of
+                  Mind Training, or <b>Lojong</b>. To understand this form of
+                  practice, it's helpful to understand what Mayhayana Buddhism
+                  is; afterall, Lojong show us how Mahayana wisdom can truly be
+                  realized.
                 </p>
+                <h3 className="lg:text-xl text-left text-2xl text-bold text-pure-blue pt-4">
+                  Mahayana Buddhism
+                </h3>
+                <p>
+                  In the simplist terms, Mahayana Buddhism followers seek to
+                  liberate themselves from suffering as well as lead others
+                  toward liberation and enlightenment. in contrast, the goal of
+                  the Theravada Buddhists is the individual attainment of
+                  Enlightenment using Buddhist meditations.
+                </p>
+                <h3 className="lg:text-xl text-left text-2xl font-titan-one text-pure-blue pt-4">
+                  Lojong
+                </h3>
+                <p>
+                Lojong is a set of 59 slogans, or techniques for training the mind. These techniques are
+                designed to guide us through every day trials and tribulations. 
+                </p>
+                <br />
+               <p>
+
+               </p>
+
+                Lojong's teachings include teachings on formless meditation, the
+                practice of "sending and receiving" (tongle), and
+                post-meditation practice - applying our meditation to our daily
+                lives. These teachings, attributed to the great 11th-century
+                Buddhist master Atisha Dipankara, became widely known after the
+                Tibetan teacher Geshe Chekawa organized and condensed them into
+                a collection of 59 mind-training sayings or reminders. Often
+                referred to simply as the Atisha Slogans, they embody the
+                essence of Mahayana practice. The Atisha Slogans are blueprints
+                for practicing the bodhisattva path in 59 simple steps. The
+                power of slogans is that they violate the Mahayana ideal of
+                loving-kindness towards us. Instead of giving general
+                instructions on how to be a true practitioner, they describe it
+                in detail. They provide specific instructions for both
+                approaching meditation and waking up to everyday life. It's easy
+                to be vaguely compassionate and generally self-aware, but when
+                we really look at what we do and how we interact with others,
+                it's a different story. As they say, the devil is in the
+                details. At the core of Buddhism are The Four Noble Truths:
+                suffering exists, there is a cause of suffering, there is an end
+                to suffering, and there is a path that leads to the end of
+                suffering.
               </div>
               <div class="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">
                 Store Consciousness
