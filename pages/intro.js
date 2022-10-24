29b58df0c169aae5993cf160dda56f55f5fb8ac5
Dan <support@stackmetric.io>
2022-10-24 15:05:02 -0500

    Redid info page

--- pages/intro.js
+++ pages/intro.js
@@ -4,75 +4,97 @@ export default function Intro() {
   return (
     <>
       <Header />
-      <div className="overflow-hidden bg-white">
-        <div className="relative mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8 grid grid-col-2">
-          <div className="absolute top-0 bottom-0 left-3/4 hidden w-screen lg:block" />
-          <div class="">
-            <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2 gap-4 ">
-              <div class="">
-                <h1 className="lg:text-4xl text-left text-2xl text-bold text-pure-blue">
-                Transforming The Mind
-                </h1>
-                <br />
-                <p>
-                  At the heart of the Zen Hero philosophy is the practice of
-                  Mind Training, or <b>Lojong</b>. To understand this form of
-                  practice, it's helpful to understand what Mayhayana Buddhism
-                  is; afterall, Lojong show us how Mahayana wisdom can truly be
-                  realized.
-                </p>
-                <h3 className="lg:text-xl text-left text-2xl text-bold text-pure-blue pt-4">
-                  Mahayana Buddhism
-                </h3>
-                <p>
-                  In the simplist terms, Mahayana Buddhism followers seek to
-                  liberate themselves from suffering as well as lead others
-                  toward liberation and enlightenment. in contrast, the goal of
-                  the Theravada Buddhists is the individual attainment of
-                  Enlightenment using Buddhist meditations.
-                </p>
-                <h3 className="lg:text-xl text-left text-2xl font-titan-one text-pure-blue pt-4">
-                  Lojong
-                </h3>
-                <p>
-                Lojong is a set of 59 slogans, or techniques for training the mind. These techniques are
-                designed to guide us through every day trials and tribulations. 
-                </p>
-                <br />
-               <p>
+      <section class="relative pt-16 bg-blueGray-50">
+<div class="container mx-auto">
+  <div class="flex flex-wrap items-center">
+    <div class="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
+      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-pure-blue">
+        <img alt="..." src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=700&amp;q=80" class="w-full align-middle rounded-t-lg" />
+        <blockquote class="relative p-8 mb-4">
+          <h4 class="text-xl font-bold text-white">
+            Great for your awesome project
+          </h4>
+          <p class="text-md font-light mt-2 text-white">
+            Putting together a page has never been easier than matching
+            together pre-made components. From landing pages presentation
+            to login areas, you can easily customise and built your pages.
+          </p>
+        </blockquote>
+      </div>
+    </div>
 
-               </p>
-
-                Lojong's teachings include teachings on formless meditation, the
-                practice of "sending and receiving" (tongle), and
-                post-meditation practice - applying our meditation to our daily
-                lives. These teachings, attributed to the great 11th-century
-                Buddhist master Atisha Dipankara, became widely known after the
-                Tibetan teacher Geshe Chekawa organized and condensed them into
-                a collection of 59 mind-training sayings or reminders. Often
-                referred to simply as the Atisha Slogans, they embody the
-                essence of Mahayana practice. The Atisha Slogans are blueprints
-                for practicing the bodhisattva path in 59 simple steps. The
-                power of slogans is that they violate the Mahayana ideal of
-                loving-kindness towards us. Instead of giving general
-                instructions on how to be a true practitioner, they describe it
-                in detail. They provide specific instructions for both
-                approaching meditation and waking up to everyday life. It's easy
-                to be vaguely compassionate and generally self-aware, but when
-                we really look at what we do and how we interact with others,
-                it's a different story. As they say, the devil is in the
-                details. At the core of Buddhism are The Four Noble Truths:
-                suffering exists, there is a cause of suffering, there is an end
-                to suffering, and there is a path that leads to the end of
-                suffering.
+    <div class="w-full md:w-6/12 px-4">
+      <div class="flex flex-wrap">
+        <div class="w-full md:w-6/12 px-4">
+          <div class="relative flex flex-col mt-4">
+            <div class="px-4 py-5 flex-auto">
+              <div class="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                <i class="fas fa-sitemap"></i>
               </div>
-              <div class="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">
-                Store Consciousness
+              <h6 class="text-xl mb-1 font-semibold">Insight Meditation</h6>
+              <p class="mb-4 text-blueGray-500">
+                Notus JS comes with a huge number of Fully Coded CSS
+                components.
+              </p>
+            </div>
+          </div>
+          <div class="relative flex flex-col min-w-0">
+            <div class="px-4 py-5 flex-auto">
+              <div class="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                <i class="fas fa-drafting-compass"></i>
               </div>
+              <h6 class="text-xl mb-1 font-semibold">
+                JavaScript Components
+              </h6>
+              <p class="mb-4 text-blueGray-500">
+                We also feature many dynamic components for React, NextJS,
+                Vue and Angular.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="w-full md:w-6/12 px-4">
+          <div class="relative flex flex-col min-w-0 mt-4">
+            <div class="px-4 py-5 flex-auto">
+              <div class="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                <i class="fas fa-newspaper"></i>
+              </div>
+              <h6 class="text-xl mb-1 font-semibold">Tonglen Meditation</h6>
+              <p class="mb-4 text-blueGray-500">
+                This extension also comes with 3 sample pages. They are
+                fully coded so you can start working instantly.
+              </p>
+            </div>
+          </div>
+          <div class="relative flex flex-col min-w-0">
+            <div class="px-4 py-5 flex-auto">
+              <div class="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                <i class="fas fa-file-alt"></i>
+              </div>
+              <h6 class="text-xl mb-1 font-semibold">Documentation</h6>
+              <p class="mb-4 text-blueGray-500">
+                Built by developers for developers. You will love how easy
+                is to to work with Notus JS.
+              </p>
             </div>
           </div>
         </div>
       </div>
+    </div>
+  </div>
+</div>
+<footer class="relative bg-blueGray-50 pt-8 pb-6 mt-2">
+  <div class="container mx-auto px-4">
+    <div class="flex flex-wrap items-center md:justify-between justify-center">
+      <div class="w-full md:w-6/12 px-4 mx-auto text-center">
+        <div class="text-sm text-blueGray-500 font-semibold py-1">
+          Made with <a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank">Notus JS</a> by <a href="https://www.creative-tim.com" class="text-blueGray-500 hover:text-blueGray-800" target="_blank"> Creative Tim</a>.
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+</section>
     </>
   );
 }
