a4964d676b018f6571ff621320a91f3a91c189de
techstack-io <81205746+techstack-io@users.noreply.github.com>
2022-10-29 01:22:20 -0500

    Added video

--- pages/intro.js
+++ pages/intro.js
@@ -3,8 +3,17 @@ import Showcase from "../components/Showcase";
 export default function Intro() {
   return (
     <>
-     <Showcase />
-
+     <center>
+     <video controls src={"img/video.mp4"} className="w-1/2 pt-12"/>
+     <img
+            src="img/box-1.svg"
+            className="inline-block p-4"
+            alt="image"
+            loading="lazy"
+            width="125"
+            height="125"
+          />
+     </center>
     </>
   );
 }
