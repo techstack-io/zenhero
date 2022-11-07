7b0740fb52b91e37c0b73b3fa49946ddb25fe443
techstack-io <81205746+techstack-io@users.noreply.github.com>
2022-11-06 20:59:18 -0600

    Added details on events page

--- components/EventItem.js
+++ components/EventItem.js
@@ -4,10 +4,10 @@ import Image from "next/image";
 export default function EventItem({ evt }) {
   return (
     <>
-      <div role="status" className="space-y-8 my-16 md:space-y-0 md:space-x-8 md:flex md:items-center">
+      <div role="status" className="space-y-8 my-28 lg:my-16 md:space-y-0 md:space-x-8 md:flex md:items-center">
         <div class="flex justify-center items-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
           <Image
-            src={evt.img ? evt.img : "/img/default.png"}
+            src={evt.image ? evt.image : "/image/default.png"}
             width={100}
             height={100}
             alt="default"
@@ -15,15 +15,15 @@ export default function EventItem({ evt }) {
         </div>
         <div class="w-full">
           <div class="font-zen-antique-soft font-bold h-2.5 w-full mb-4">
-              {evt.name}
+              {evt.name} 
           </div>
+          <div className="whitespace-nowrap text-left text-sm text-gray-500">
+                    <time dateTime={evt.date}>{evt.time}</time>
+                  </div>
           <div class="h-2 max-w-[480px] mb-2.5">
               {evt.description}
           </div>
-          <div class="h-2 mb-2.5"></div>
-          <div class="h-2 max-w-[440px] mb-2.5"></div>
-          <div class="h-2 max-w-[460px] mb-2.5"></div>
-          <div class="h-2 max-w-[360px]"></div>
+
         </div>
         <span class="sr-only">Loading...</span>
       </div>
