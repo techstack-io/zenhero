7eb8ea0f25b97776e0c11f311ae74d3f37071c6b
Dan <support@stackmetric.io>
2022-11-21 15:21:47 -0600

    added toastify

--- components/Register.js
+++ components/Register.js
@@ -1,31 +1,33 @@
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useState, useEffect, useContext } from "react";
 import Link from "next/link";
 
 function Register() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    if (password !== PasswordConfirm) {
-      toast.error("Passwords do not match. Please try again.");
+    if (password !== passwordConfirm) {
+      toast.error('Passwords do not match. Please try again')
       return
     }
   };
+
   return (
     <>
-      <section class="">
+      <section className="">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Create an Account
               </h1>
+              <ToastContainer />
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
                   <label
@@ -62,7 +64,7 @@ function Register() {
                 </div>
                 <div>
                   <label
-                    for="password"
+                    htmlFor="password"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Password
@@ -89,6 +91,7 @@ function Register() {
                     type="password"
                     name="passwordConfirm"
                     id="passwordConfirm"
+                    value={passwordConfirm}
                     onChange={(e) => setPasswordConfirm(e.target.value)}
                     placeholder="••••••••"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
